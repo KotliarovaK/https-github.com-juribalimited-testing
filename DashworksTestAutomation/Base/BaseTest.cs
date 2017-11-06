df4691751a674539206a3b8f5e3961116e1cff96
sclode <stuart.clode@juriba.com>
2017-11-06 15:58:53 +0000

    DAS-11040 SeleniumWebDriverTest - Configure Chrome Browser Window Size - Potential fix for screen resolution issue?

--- DashworksTestAutomation/Base/BaseTest.cs
+++ DashworksTestAutomation/Base/BaseTest.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using DashworksTestAutomation.Providers;
+﻿using DashworksTestAutomation.Providers;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Remote;
+using System;
 
 namespace DashworksTestAutomation.Base
 {
-    class BaseTest : IBaseTest
+    internal class BaseTest : IBaseTest
     {
         public RemoteWebDriver Driver { get; set; }
 
@@ -17,16 +17,20 @@ namespace DashworksTestAutomation.Base
             switch (Browser.Type)
             {
                 case "Chrome":
-                    //ChromeOptions options = new ChromeOptions();
-                    //options.AddArgument($"--window-size={Browser.Resolution}");
-                    //var driver = new ChromeDriver(options);
-                    return new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    options.AddArgument("--window-position=0,0");
+                    options.AddArgument(string.Format("--window-size={0}", Browser.Resolution));
+                    return new ChromeDriver(options);
+
                 case "Firefox":
                     return new FirefoxDriver();
+
                 case "InternetExplorer":
                     return new InternetExplorerDriver();
+
                 case "Edge":
                     return new EdgeDriver();
+
                 default:
                     throw new Exception($"Browser type '{Browser.Type}' was not identified");
             }
