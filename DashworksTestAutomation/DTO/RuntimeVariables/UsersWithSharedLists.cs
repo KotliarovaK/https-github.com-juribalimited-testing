7882c44c9a702e53144e3cf3930886639417dff2
Vitalii Dolotov <devnull@localhost>
2018-01-05 22:58:27 +0200

    Emproved Delete_Newly_Created_List step to also remove shared lists

---
+++ DashworksTestAutomation/DTO/RuntimeVariables/UsersWithSharedLists.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.DTO.RuntimeVariables
+{
+    //If we assigne list to any user we nned to add it to this list
+    //to be able to remove list after test execution
+    class UsersWithSharedLists
+    {
+        public List<string> Value { get; set; }
+    }
+}
