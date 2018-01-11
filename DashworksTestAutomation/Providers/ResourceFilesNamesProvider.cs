3462b1d9b4ac186505deda051d1a6a43fad49e37
Vitalii Dolotov <devnull@localhost>
2018-01-11 20:16:34 +0200

    Added ResourceFilesNamesProvider provider

---
+++ DashworksTestAutomation/Providers/ResourceFilesNamesProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.Providers
+{
+    public static class ResourceFilesNamesProvider
+    {
+        private static string ResourcesFolderName => "\\Resources";
+
+        public static string CorrectFile => $"{ResourcesFolderName}\\CorrectFile.png";
+        public static string IncorrectFile => $"{ResourcesFolderName}\\IncorrectFile.zip";
+    }
+}
