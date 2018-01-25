52c1ec8a9a5a8227f939f2d9ce8283661c9affb9
Vitalii Dolotov <devnull@localhost>
2018-01-25 15:29:32 +0200

    Added part of test for DAS-11689

--- DashworksTestAutomation/Pages/Evergreen/BaseDashboardPage.cs
+++ DashworksTestAutomation/Pages/Evergreen/BaseDashboardPage.cs
@@ -59,6 +59,9 @@ namespace DashworksTestAutomation.Pages.Evergreen
         [FindsBy(How = How.XPath, Using = ".//div[@class='ag-body-container']")]
         public IWebElement TableBody { get; set; }
 
+        [FindsBy(How = How.XPath, Using = ".//div[@class='ag-body']")]
+        public IWebElement TableContent { get; set; }
+
         [FindsBy(How = How.XPath, Using = ".//div[@id='content']//div[@class='empty-message ng-star-inserted']")]
         public IWebElement NoResultsFoundMessage { get; set; }
 
