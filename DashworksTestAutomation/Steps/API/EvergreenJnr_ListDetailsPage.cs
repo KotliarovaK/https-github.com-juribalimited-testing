86a343dd807483fbc90ef41a41676f4918508185
Vitalii Dolotov <devnull@localhost>
2018-02-22 18:11:56 +0200

    - added new API step - created API folders - fixed test

---
+++ DashworksTestAutomation/Steps/API/EvergreenJnr_ListDetailsPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DashworksTestAutomation.DTO.RuntimeVariables;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.API
+{
+    [Binding]
+    internal class EvergreenJnr_ListDetailsPage
+    {
+        private readonly ResponceDetails _responce;
+
+        public EvergreenJnr_ListDetailsPage(ResponceDetails responce)
+        {
+            _responce = responce;
+        }
+
+        [Then(@"""(.*)"" field display state is ""(.*)"" on Details tab API")]
+        public void ThenFieldIsDisplayedOnDetailsTab(string fieldName, string state)
+        {
+            var content = _responce.Value.Content;
+            var allItems = JsonConvert.DeserializeObject<JObject>(content)["metadata"];
+            try
+            {
+                var item = allItems.First(x => x["friendlyName"].ToString().Equals(fieldName));
+                Assert.AreEqual(state, item["visible"].ToString(), $"Incorrect display state for {fieldName}");
+            }
+            catch
+            {
+                Assert.AreEqual(state, "False", $"Incorrect display state for {fieldName}");
+            }
+        }
+
+        [Then(@"""(.*)"" text displayed for ""(.*)"" empty fields")]
+        public void ThenTextDisplayedForEmptyFields(string text, string sectionName)
+        {
+            var content = _responce.Value.Content;
+            var allFields = JsonConvert.DeserializeObject<JObject>(content)["results"];
+            foreach (var pair in allFields)
+            {
+                if (pair.ToString().Contains("address2") || pair.ToString().Contains("address3") ||
+                    pair.ToString().Contains("address4") ||
+                    pair.ToString().Contains("pendingStickyDepartmentMessage") ||
+                    pair.ToString().Contains("pendingStickyLocationMessage"))
+                    continue;
+                Assert.IsFalse(!string.IsNullOrEmpty(pair.Last.ToString()),
+                    "'Unknown' text is displayed for field ");
+            }
+        }
+    }
+}
---
+++ DashworksTestAutomation/Steps/API/EvergreenJnr_ProfilePage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DashworksTestAutomation.DTO.RuntimeVariables;
+using DashworksTestAutomation.Providers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.API
+{
+    [Binding]
+    internal class EvergreenJnr_ProfilePage
+    {
+        private readonly RestWebClient _client;
+
+        public EvergreenJnr_ProfilePage(RestWebClient client)
+        {
+            _client = client;
+        }
+
+        [Then(@"default list page Size is ""(.*)"" and Cache ""(.*)""")]
+        public void ThenDefaultListPageSizeIsAndCache(int pageSize, int pageCache)
+        {
+            var requestUri = $"{UrlProvider.RestClientBaseUrl}security/userprofile";
+            var request = new RestRequest(requestUri);
+
+            request.AddParameter("Host", UrlProvider.RestClientBaseUrl);
+            request.AddParameter("Origin", UrlProvider.Url.TrimEnd('/'));
+            request.AddParameter("Referer", UrlProvider.EvergreenUrl);
+
+            var response = _client.Value.Get(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception($"Unable to execute request. URI: {requestUri}");
+
+            var content = response.Content;
+
+            var pageOptions = JsonConvert.DeserializeObject<JObject>(content);
+            var listPageSize = Convert.ToInt32(pageOptions["gridPageSize"]);
+            var listPageToCache = Convert.ToInt32(pageOptions["gridPageCache"]);
+            Assert.AreEqual(pageSize, listPageSize, "Incorrect Page Size on Account page");
+            Assert.AreEqual(pageCache, listPageToCache, "Incorrect Cache Size on Account page");
+        }
+    }
+}
