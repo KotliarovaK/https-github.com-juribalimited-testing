14d0707a0c2a0b4ace3845a0649aa1731f68c618
Vitalii Dolotov <devnull@localhost>
2018-04-04 15:08:17 +0300

    Added the DTO files

---
+++ DashworksTestAutomation/DTO/Projects/DetailsDto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.DTO.Projects
+{
+    public class DetailsDto
+    {
+        public DefaultReadinessForOnboardedApplicationsEnum DefaultReadinessForOnboardedApplications;
+        public DefaultValueForShowLinkedObjectsEnum DefaultValueForShowLinkedObjects;
+        public DefaultViewForProjectObjectApplicationsTab1Enum DefaultViewForProjectObjectApplicationsTab1;
+        public DefaultViewForProjectObjectApplicationsTab2Enum DefaultViewForProjectObjectApplicationsTab2;
+        public DefaultValueForApplicationRationalizationEnum DefaultValueForApplicationRationalization;
+        public bool ShowOriginalApplicationColumnOnApplicationDashboards { get; set; }
+        public bool IncludeSiteNameInApplicationName { get; set; }
+        public bool OnboardNotApplicableApplications { get; set; }
+        public bool OnboardInstalledApplicationsByAssociation { get; set; }
+        public bool OnboardEntitledApplicationsByAssociation { get; set; }
+        public OnboardUsedApplicationsByAssociationToEnum OnboardUsedApplicationsByAssociationTo;
+        //public string MasterHTMLEmailTemplate { get; set; }
+        //Attachments
+        public string TaskEmailCcEmailAddress { get; set; }
+        public string TaskEmailBccEmailAddress { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+
+    public enum DefaultReadinessForOnboardedApplicationsEnum
+    {
+        [Description("Out Of Scope")]
+        OutOfScope,
+        Blue,
+        [Description("Light Blue")]
+        LightBlue,
+        Red,
+        Brown,
+        Amber,
+        [Description("Really Extremely Orange")]
+        ReallyExtremelyOrange,
+        Purple,
+        Green,
+        Grey,
+        None
+    }
+
+    public enum DefaultValueForShowLinkedObjectsEnum
+    {
+        Yes,
+        No
+    }
+
+    public enum DefaultViewForProjectObjectApplicationsTab1Enum
+    {
+        [Description("Current State")]
+        CurrentState,
+        [Description("Target State")]
+        TargetState,
+        Comparison
+    }
+
+    public enum DefaultViewForProjectObjectApplicationsTab2Enum
+    {
+        [Description("Group by Object")]
+        GroupbyObject,
+        Alphabetical,
+        Consolidated,
+        Readiness
+    }
+
+    public enum DefaultValueForApplicationRationalizationEnum
+    {
+        Retire,
+        Keep,
+        Uncategorized
+    }
+
+    public enum OnboardUsedApplicationsByAssociationToEnum
+    {
+        Computer,
+        User,
+        [Description("Do not onboard")]
+        DoNotOnboard
+    }
+}
---
+++ DashworksTestAutomation/DTO/Projects/ProjectDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.DTO.Projects
+{
+    public class ProjectDto
+    {
+        public string ProjectName { get; set; }
+        public string ProjectShortName { get; set; }
+        public string ProjectDescription { get; set; }
+        public ProjectTypeEnum ProjectType;
+        public DefaultLanguageEnum DefaultLanguage;
+        public DetailsDto Details { get; set; }
+    }
+
+    public enum ProjectTypeEnum
+    {
+        [Description("Computer Scheduled Project")]
+        ComputerScheduledProject,
+        [Description("User Scheduled Project")]
+        UserScheduledProject,
+        [Description("Mailbox Scheduled Project")]
+        MailboxScheduledProject
+    }
+
+    public enum DefaultLanguageEnum
+    {
+        Arabic,
+        [Description("British English")]
+        BritishEnglish,
+        English,
+        Polish,
+        Russian,
+        [Description("Mailbox Scheduled Project")]
+        MailboxScheduledProject
+    }
+}
