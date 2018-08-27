bffbdf8479c210402b58f00135c19820141f6a88
Hannah Ilchenko <devnull@localhost>
2018-08-27 11:39:03 +0300

    Updated test for DAS12883

--- DashworksTestAutomation/Steps/Dashworks/EvergreenJnr_DetailsPage.cs
+++ DashworksTestAutomation/Steps/Dashworks/EvergreenJnr_DetailsPage.cs
@@ -43,7 +43,6 @@ namespace DashworksTestAutomation.Steps.Dashworks
         {
             var detailsPage = _driver.NowAt<DetailsPage>();
             detailsPage.NavigateToSectionByName(sectionName);
-            _driver.WaitForDataLoading();
         }
 
         [When(@"User clicks ""(.*)"" link on the Details Page")]
@@ -58,6 +57,7 @@ namespace DashworksTestAutomation.Steps.Dashworks
         {
             var detailsPage = _driver.NowAt<DetailsPage>();
 
+            _driver.WaitForDataLoading();
             if (!detailsPage.OpenedSection.Displayed())
             {
                 Assert.IsTrue(detailsPage.NoFoundContent.Displayed());
@@ -339,7 +339,7 @@ namespace DashworksTestAutomation.Steps.Dashworks
         public void ThenBucketPop_UpHasStandardSizeOnTheDetailsPage()
         {
             var detailsPage = _driver.NowAt<DetailsPage>();
-            Assert.AreEqual("1536px", detailsPage.GetInstalledBucketWindowWidth());
+            Assert.AreEqual("1638", detailsPage.GetInstalledBucketWindowWidth().Split('.').First());
         }
 
         [When(@"User enters ""(.*)"" text in the Filter field")]
