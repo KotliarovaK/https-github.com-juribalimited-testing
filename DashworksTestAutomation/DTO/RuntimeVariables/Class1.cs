135af117476993fd6f677122ec0d2ed15b3a8610
Kateryna Kotliarova <Kateryna.Kotliarova@juriba.com>
2019-06-04 10:13:48 +0300

    Added creating Automation step via API

---
+++ DashworksTestAutomation/DTO/RuntimeVariables/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DashworksTestAutomation.DTO.Evergreen.Admin.Automations;
+using DashworksTestAutomation.DTO.Evergreen.Admin.CapacityUnits;
+
+namespace DashworksTestAutomation.DTO.RuntimeVariables
+{
+    //Created capacity unit names are stored here
+    public class Automation
+    {
+        public List<AutomationsDto> Value { get; set; }
+
+        public Automation()
+        {
+            Value = new List<AutomationsDto>();
+        }
+    }
+}
