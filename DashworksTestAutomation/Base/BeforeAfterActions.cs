71eb9abaa5cc1a49e74ab87513b90f0f53c85c4c
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-07-05 13:47:04 +0300

    Added BambooUtil class for automatic Unleash tests

--- DashworksTestAutomation/Base/BeforeAfterActions.cs
+++ DashworksTestAutomation/Base/BeforeAfterActions.cs
@@ -8,10 +8,13 @@ using BoDi;
 using DashworksTestAutomation.Extensions;
 using DashworksTestAutomation.Providers;
 using DashworksTestAutomation.Utils;
+using HtmlAgilityPack;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
+using RestSharp;
+using RestSharp.Authenticators;
 using TechTalk.SpecFlow;
 
 namespace DashworksTestAutomation.Base
@@ -28,9 +31,86 @@ namespace DashworksTestAutomation.Base
             _scenarioContext = scenarioContext;
         }
 
+        [BeforeTestRun]
+        public void BeforeTestRun()
+        {
+            if (!Browser.RemoteDriver.Equals("local"))
+                BambooUtil.GetAllQuarantinedTests();
+        }
+
         [BeforeScenario]
         public void OnStartUp()
         {
+            //try
+            //{
+            //    if (string.IsNullOrEmpty(BambooProvider.BuildKey))
+            //        return;
+
+            //    var projAndBuild = $"{BambooProvider.ProjectKey}-{BambooProvider.BuildKey}";
+            //    var client = new RestClient
+            //    {
+            //        BaseUrl = new Uri(BambooProvider.BaseUrl),
+            //        Authenticator = new HttpBasicAuthenticator(BambooProvider.Username, BambooProvider.Password)
+            //    };
+
+            //    var request = new RestRequest(Method.GET) {Resource = $"/browse/{projAndBuild}"};
+            //    request.AddHeader("Accept", "application/json");
+            //    request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            //    request.RequestFormat = DataFormat.Json;
+            //    IRestResponse response = client.Execute(request);
+
+            //    #region Get previous build ID
+
+            //    HtmlDocument doc = new HtmlDocument();
+            //    string html = response.Content;
+            //    doc.LoadHtml(html);
+            //    var buildIdElement = doc.DocumentNode.SelectNodes("//a[@class='statusIndicator']");
+            //    var prevBuildId = int.Parse(buildIdElement.First().GetAttributeValue("href", null).Split('/').Last().Split('-').Last()) - 1;
+
+            //    #endregion
+
+            //    #region Get all quarantined Tests 
+
+            //    request = new RestRequest(Method.GET) {Resource = $"/browse/{projAndBuild}-{prevBuildId}/test"};
+            //    request.AddHeader("Accept", "application/json");
+            //    request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            //    request.RequestFormat = DataFormat.Json;
+            //    response = client.Execute(request);
+
+            //    html = response.Content;
+            //    doc.LoadHtml(html);
+
+            //    var quarantinedTests =
+            //        doc.DocumentNode.SelectNodes("//table[@id='skipped-tests']//a[@class='test-name']");
+
+            //    List<KeyValuePair<string, string>> testIdsWithNames = new List<KeyValuePair<string, string>>();
+            //    foreach (HtmlNode node in quarantinedTests)
+            //    {
+            //        var id = node.GetAttributeValue("href", null).Split('/').Last();
+            //        var name = node.InnerText;
+            //        testIdsWithNames.Add(new KeyValuePair<string, string>(id, name));
+            //    }
+
+            //    #endregion
+
+            //    foreach (KeyValuePair<string, string> pair in testIdsWithNames)
+            //    {
+            //        request = new RestRequest(Method.POST)
+            //        {
+            //            Resource = $"/rest/api/latest/plan/{projAndBuild}-{prevBuildId + 1}/test/{pair.Key}/unleash"
+            //        };
+            //        request.AddHeader("Accept", "application/json");
+            //        request.AddHeader("Content-Type", "application/json; charset=utf-8");
+
+            //        request.RequestFormat = DataFormat.Json;
+
+            //        response = client.Execute(request);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
             List<string> testTags = TestContext.CurrentContext.Test.Properties["Category"].Select(x => x.ToString()).ToList();
             LockCategory.AwaitTags(testTags);
             LockCategory.AddTags(testTags);
@@ -55,30 +135,16 @@ namespace DashworksTestAutomation.Base
 
                 try
                 {
-                    //if (Browser.RemoteDriver.Equals("sauceLabs"))
-                    //{
-                    //    bool passed = TestContext.CurrentContext.Result.Outcome.Status ==
-                    //                  TestStatus.Passed;
-
-                    //    try
-                    //    {
-                    //        // Logs the result to Sauce Labs
-                    //        ((IJavaScriptExecutor)driver).ExecuteScript(
-                    //            "sauce:job-result=" + (passed ? "passed" : "failed"));
-                    //    }
-                    //    finally
-                    //    {
-                    //        Console.WriteLine(
-                    //            $"SauceOnDemandSessionID={((CustomRemoteWebDriver)driver).getSessionId()} job-name={TestContext.CurrentContext.Test.MethodName}");
-                    //    }
-                    //}
-
                     var testStatus = GetTestStatus();
                     if (!string.IsNullOrEmpty(testStatus) && testStatus.Equals("Failed"))
                     {
                         var testName = GetTestName();
                         if (!string.IsNullOrEmpty(testName))
                             driver.CreateScreenshot(testName);
+                    }
+                    else
+                    {
+                        BambooUtil.UnleashTest(GetTestName());
                     }
 
                     Logger.Write($"Closing window at: {driver.Url}");
---
+++ DashworksTestAutomation/Providers/BambooProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.Providers
+{
+    public static class BambooProvider
+    {
+        public static string BaseUrl => "https://panda.corp.juriba.com";
+        public static readonly string Username = ConfigurationManager.AppSettings["bamboo.username"];
+        public static readonly string Password = ConfigurationManager.AppSettings["bamboo.password"];
+        public static readonly string ProjectKey = ConfigurationManager.AppSettings["bamboo.projectKey"];
+        public static readonly string BuildKey = ConfigurationManager.AppSettings["bamboo.buildKey"];
+    }
+}
