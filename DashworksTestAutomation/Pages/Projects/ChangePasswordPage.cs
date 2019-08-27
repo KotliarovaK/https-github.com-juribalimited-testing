2a8ca55fa9242b0278a2197b5a709ed8a16d33bb
Sergiy.Zinchovski@juriba.com <Sergiy.Zinchovski@juriba.com>
2019-08-27 19:16:33 +0300

    Created autotest for DAS17586

---
+++ DashworksTestAutomation/Pages/Projects/ChangePasswordPage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DashworksTestAutomation.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace DashworksTestAutomation.Pages.Projects
+{
+    internal class ChangePasswordPage : SeleniumBasePage
+    {
+        [FindsBy(How = How.XPath, Using = ".//div/h1")]
+        public IWebElement PageTitle { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//input[contains(@id, 'ExistingPwd')]")]
+        public IWebElement CurrentPasswordInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//input[contains(@id, 'NewPwd')]")]
+        public IWebElement NewPasswordInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//input[contains(@id, 'ConfirmPwd')]")]
+        public IWebElement ConfirmNewPasswordInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//input[@value='Change Password']")]
+        public IWebElement ChangePasswordButton { get; set; }
+
+
+        public override List<By> GetPageIdentitySelectors()
+        {
+            return new List<By>
+            {
+                SelectorFor(this, p => p.PageTitle)
+            };
+        }
+    }
+}
---
+++ DashworksTestAutomation/Pages/Projects/UserProfilePage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DashworksTestAutomation.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace DashworksTestAutomation.Pages.Projects
+{
+    internal class UserProfilePage : SeleniumBasePage
+    {
+        [FindsBy(How = How.XPath, Using = ".//div/h1")]
+        public IWebElement PageTitle { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//input[@value='Change Password']")]
+        public IWebElement ChangePasswordButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//li[@role='alert']")]
+        public IWebElement NotificationBanner { get; set; }
+
+        public override List<By> GetPageIdentitySelectors()
+        {
+            return new List<By>
+            {
+                SelectorFor(this, p => p.PageTitle)
+            };
+        }
+    }
+}
