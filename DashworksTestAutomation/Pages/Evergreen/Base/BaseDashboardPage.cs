ff1cc3fa42c70e3e58fd591f630e10a53ff6819e
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-10-17 19:38:03 +0300

    Fix for redines test

--- DashworksTestAutomation/Pages/Evergreen/Base/BaseDashboardPage.cs
+++ DashworksTestAutomation/Pages/Evergreen/Base/BaseDashboardPage.cs
@@ -103,9 +103,6 @@ namespace DashworksTestAutomation.Pages.Evergreen.Base
         [FindsBy(How = How.XPath, Using = ".//mat-dialog-container//button[contains(@class, 'mat-primary')]")]
         public IWebElement DeleteButtonInPopUp { get; set; }
 
-        [FindsBy(How = How.XPath, Using = ".//mat-dialog-container//button[@class='mat-raised-button']")]
-        public IWebElement CancelButtonInPopUp { get; set; }
-
         [FindsBy(How = How.XPath,
             Using = "//div[contains(@class, 'notification')]//span[text()='UPDATE']/ancestor::button")]
         public IWebElement UpdateButtonOnAmberMessage { get; set; }
