1ca91ef024d74aea711c86392456eb3670a245e8
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-10-29 14:07:16 +0200

    Updated NamedDropdownSelector selector

--- DashworksTestAutomation/Pages/Evergreen/Base/BaseDashboardPage.cs
+++ DashworksTestAutomation/Pages/Evergreen/Base/BaseDashboardPage.cs
@@ -16,7 +16,7 @@ namespace DashworksTestAutomation.Pages.Evergreen.Base
 
         public const string DatepickerCellSelector = "//td[contains(@class,'cell')]";
 
-        private string NamedDropdownSelector = ".//mat-select[@aria-label='{0}' or @automation='{0}']";
+        private string NamedDropdownSelector = ".//mat-select[@aria-label='{0}' or @automation='{0}']|//label[text()='{0}']/ancestor::span//preceding-sibling::mat-select";
 
         public const string ColorItem = ".//div[@class='status']";
 
