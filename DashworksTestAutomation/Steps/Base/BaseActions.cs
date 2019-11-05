3fbc2714adcf0064d751ea55a49fef6c10f7768d
Sergiy.Zinchovski@juriba.com <Sergiy.Zinchovski@juriba.com>
2019-11-05 12:08:33 +0200

    Retry and change NaviagateTo

--- DashworksTestAutomation/Steps/Base/BaseActions.cs
+++ DashworksTestAutomation/Steps/Base/BaseActions.cs
@@ -56,7 +56,7 @@ namespace DashworksTestAutomation.Steps.Base
         [When(@"User navigates to '(.*)' url via address line")]
         public void ThenUserNavigatesToTheSpecificUrlViaAddressLine(string url)
         {
-            _driver.Navigate().GoToUrl($"{UrlProvider.EvergreenUrl}#/{url}");
+            _driver.NavigateToUrl($"{UrlProvider.EvergreenUrl}#/{url}");
         }
     }
 }
