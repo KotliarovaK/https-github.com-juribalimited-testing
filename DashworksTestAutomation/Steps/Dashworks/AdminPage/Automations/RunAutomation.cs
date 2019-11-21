edd03f1e35d99004b17f7ec0fd56789748bd21bd
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-11-21 17:16:24 +0200

    Added testInfo class

---
+++ DashworksTestAutomation/Steps/Dashworks/AdminPage/Automations/RunAutomation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.Dashworks.AdminPage.Automations
+{
+    [Binding]
+    public class RunAutomation : SpecFlowContext
+    {
+        private readonly RemoteWebDriver _driver;
+
+        public RunAutomation(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+         
+    }
+}
