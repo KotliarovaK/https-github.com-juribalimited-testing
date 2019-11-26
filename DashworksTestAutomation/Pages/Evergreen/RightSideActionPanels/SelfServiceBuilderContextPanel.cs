bb59f1359d8e0b0d00673016e35b83ec76a4a90f
andrewd <Andrew.Dzimiskyi@juriba.com>
2019-11-26 18:48:33 +0200

    PageObject of SelfService Builder Context Panel Created

---
+++ DashworksTestAutomation/Pages/Evergreen/RightSideActionPanels/SelfServiceBuilderContextPanel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DashworksTestAutomation.Base;
+using DashworksTestAutomation.Extensions;
+using DashworksTestAutomation.Pages.Evergreen.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace DashworksTestAutomation.Pages.Evergreen.RightSideActionPanels
+{
+    class SelfServiceBuilderContextPanel : BaseRightSideActionsPanel
+    {
+        [FindsBy(How = How.XPath, Using = ".//button[@aria-label='Collapse All']")]
+        public IWebElement CollapseAllButton { get; set; }
+
+
+        public IWebElement ContextPanelPageArrow(String contextPanelPageName)
+        {
+            var selector = $".//div[text()='{contextPanelPageName}']/../../..//button[@class='btn-open-close ng-star-inserted']";
+            Driver.WaitForElementToBeDisplayed(By.XPath(selector));
+            return Driver.FindElement(By.XPath(selector));
+        }
+        
+        public IWebElement ContextPanelPageAddItem(String contextPanelPageName)
+        {
+            var selector = $".//div[text()='{contextPanelPageName}']/../../..//i[@class='material-icons mat-item_add']";
+            Driver.WaitForElementToBeDisplayed(By.XPath(selector));
+            return Driver.FindElement(By.XPath(selector));
+        }
+
+        public IWebElement ContextPanelPageCogMenu(String contextPanelPageName)
+        {
+            var selector = $".//div[text()='{contextPanelPageName}']/../../..//div[@class='menu-wrapper']";
+            Driver.WaitForElementToBeDisplayed(By.XPath(selector));
+            return Driver.FindElement(By.XPath(selector));
+        }
+
+        public IWebElement ContextPanelPageSubCogMenu(String contextPanelPageName, String componentHeading, String componentName)
+        {
+            var selector = $"(.//div[@class='page-level-name' and text()='{contextPanelPageName}']/parent::div/parent::div/following::div//div[@class='page-level-label' and text()='{componentHeading}']/following-sibling::div[@class='page-level-name' and text()='{componentName}']/parent::div/following::div[@class='menu-wrapper'])[1]";
+            Driver.WaitForElementToBeDisplayed(By.XPath(selector));
+            return Driver.FindElement(By.XPath(selector));
+        }
+
+        public void ClickOnExpandOrCollapseAllButtonSelfServiceBuilderContextPanel(Boolean collapsExpand)
+        {
+            
+            if (collapsExpand && )
+            {
+
+            }
+        }
+    }
+}
---
+++ DashworksTestAutomation/Steps/RightSideActionsPanel/EvergreenJnr_SelfServiceBuilderContextPanel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using DashworksTestAutomation.Extensions;
+using DashworksTestAutomation.Pages.Evergreen;
+using DashworksTestAutomation.Pages.Evergreen.AdminDetailsPages;
+using DashworksTestAutomation.Pages.Evergreen.Base;
+using DashworksTestAutomation.Pages.Evergreen.DetailsTabsMenu;
+using DashworksTestAutomation.Pages.Evergreen.ItemDetails;
+using DashworksTestAutomation.Pages.Evergreen.RightSideActionPanels;
+using DashworksTestAutomation.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.RightSideActionsPanel
+{
+    [Binding]
+    class EvergreenJnr_SelfServiceBuilderContextPanel : SpecFlowContext
+    {
+        private readonly RemoteWebDriver _driver;
+
+        public EvergreenJnr_SelfServiceBuilderContextPanel(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [When(@"User clicks on Expand All button in Self Service Builder Context Panel")]
+        public void WhenUserClicksOnExpandAllInSelfServiceBuilderContextPanel()
+        {
+            var dashboardPage = _driver.NowAt<BaseGridPage>();
+            _driver.WaitForDataLoading();
+            
+        }
+
+        [When(@"User clicks on Collapse All button in Self Service Builder Context Panel")]
+        public void WhenUserClicksOnCollapseAllInSelfServiceBuilderContextPanel()
+        {
+            var dashboardPage = _driver.NowAt<BaseGridPage>();
+            _driver.WaitForDataLoading();
+            
+            }
+        }
+    }
+}
