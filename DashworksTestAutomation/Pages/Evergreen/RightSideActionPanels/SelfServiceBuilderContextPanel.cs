963c58091d2241752d5c20ee0e2392aa29f6ab94
andrewd <Andrew.Dzimiskyi@juriba.com>
2019-11-28 16:46:34 +0200

    Added steps for SS Context Panel

--- DashworksTestAutomation/Pages/Evergreen/RightSideActionPanels/SelfServiceBuilderContextPanel.cs
+++ DashworksTestAutomation/Pages/Evergreen/RightSideActionPanels/SelfServiceBuilderContextPanel.cs
@@ -13,45 +13,99 @@ namespace DashworksTestAutomation.Pages.Evergreen.RightSideActionPanels
 {
     class SelfServiceBuilderContextPanel : BaseRightSideActionsPanel
     {
-        [FindsBy(How = How.XPath, Using = ".//button[@aria-label='Collapse All']")]
-        public IWebElement CollapseAllButton { get; set; }
+        [FindsBy(How = How.XPath, Using = ".//button[@class='btn-expand-collapse mat-raised-button mat-button-base ng-star-inserted']")]
+        public IWebElement CollapseExpandAllButton { get; set; }
 
 
         public IWebElement ContextPanelPageArrow(String contextPanelPageName)
         {
-            var selector = $".//div[text()='{contextPanelPageName}']/../../..//button[@class='btn-open-close ng-star-inserted']";
+            var selector = $".//div[text()='{contextPanelPageName}']/../../..//button[@class='btn-open-close ng-star-inserted']//i";
             Driver.WaitForElementToBeDisplayed(By.XPath(selector));
             return Driver.FindElement(By.XPath(selector));
         }
         
-        public IWebElement ContextPanelPageAddItem(String contextPanelPageName)
+        public IWebElement ContextPanelPageAddItemButton(String contextPanelPageName)
         {
             var selector = $".//div[text()='{contextPanelPageName}']/../../..//i[@class='material-icons mat-item_add']";
             Driver.WaitForElementToBeDisplayed(By.XPath(selector));
             return Driver.FindElement(By.XPath(selector));
         }
 
-        public IWebElement ContextPanelPageCogMenu(String contextPanelPageName)
+        public IWebElement ContextPanelPageCogMenuButton(String contextPanelPageName)
         {
             var selector = $".//div[text()='{contextPanelPageName}']/../../..//div[@class='menu-wrapper']";
             Driver.WaitForElementToBeDisplayed(By.XPath(selector));
             return Driver.FindElement(By.XPath(selector));
         }
 
-        public IWebElement ContextPanelPageSubCogMenu(String contextPanelPageName, String componentHeading, String componentName)
+        public IWebElement ContextPanelPageSubCogMenuButton(String contextPanelPageName, String componentHeading, String componentName)
         {
             var selector = $"(.//div[@class='page-level-name' and text()='{contextPanelPageName}']/parent::div/parent::div/following::div//div[@class='page-level-label' and text()='{componentHeading}']/following-sibling::div[@class='page-level-name' and text()='{componentName}']/parent::div/following::div[@class='menu-wrapper'])[1]";
             Driver.WaitForElementToBeDisplayed(By.XPath(selector));
             return Driver.FindElement(By.XPath(selector));
         }
 
-        public void ClickOnExpandOrCollapseAllButtonSelfServiceBuilderContextPanel(Boolean collapsExpand)
-        {
-            
-            if (collapsExpand && )
-            {
 
+
+        public Boolean GetCollapseExpandSelfServiceBuilderPagesButtonState()
+        {
+            if (CollapseExpandAllButton.GetAttribute("aria-label").Contains("Collapse All"))
+            {
+                return true;
+            } else
+            {
+                return false;
             }
+        }
+
+        public void CollapseExpandSelfServiceBuilderContextPanelElements(Boolean collapseExpand)
+        {
+            if (collapseExpand != GetCollapseExpandSelfServiceBuilderPagesButtonState())
+            {
+                CollapseExpandAllButton.Click();
+            }
+
+        }
+
+        public Boolean GetCollapseExpandSelfServiceBuilderPageButtonState(String contextPanelPageName)
+        {
+            if (ContextPanelPageArrow(contextPanelPageName).GetAttribute("class").Contains("material-icons mat-arrow_down"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CollapseExpandSelfServiceBuilderPageOnContextPanel(String contextPanelPageName, Boolean collapseExpand)
+        {
+            if (collapseExpand != GetCollapseExpandSelfServiceBuilderPageButtonState(contextPanelPageName))
+            {
+                ContextPanelPageArrow(contextPanelPageName).Click();
+            }
+
+        }
+
+        public void ClickOnContextPanelPageAddItemButton(String contextPanelPageName)
+        {
+            ContextPanelPageAddItemButton(contextPanelPageName).Click();
+        }
+
+        public void ClickOnContextPanelPageCogMenuButton(String contextPanelPageName)
+        {
+            ContextPanelPageCogMenuButton(contextPanelPageName).Click();
+        }
+
+        public void ClickOnContextPanelPageSubCogMenuButton(String contextPanelPageName, String componentHeading, String componentName)
+        {
+            ContextPanelPageSubCogMenuButton(contextPanelPageName, componentHeading, componentName).Click();
+        }
+
+        public void ClickOnCreatePageButton()
+        {
+            ContextPanelPageAddItemButton().Click();
         }
     }
 }
--- DashworksTestAutomation/Steps/RightSideActionsPanel/EvergreenJnr_SelfServiceBuilderContextPanel.cs
+++ DashworksTestAutomation/Steps/RightSideActionsPanel/EvergreenJnr_SelfServiceBuilderContextPanel.cs
@@ -29,18 +29,78 @@ namespace DashworksTestAutomation.Steps.RightSideActionsPanel
         [When(@"User clicks on Expand All button in Self Service Builder Context Panel")]
         public void WhenUserClicksOnExpandAllInSelfServiceBuilderContextPanel()
         {
-            var dashboardPage = _driver.NowAt<BaseGridPage>();
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
             _driver.WaitForDataLoading();
-            
+            dashboardPage.CollapseExpandSelfServiceBuilderContextPanelElements(true);
+
         }
 
         [When(@"User clicks on Collapse All button in Self Service Builder Context Panel")]
         public void WhenUserClicksOnCollapseAllInSelfServiceBuilderContextPanel()
         {
-            var dashboardPage = _driver.NowAt<BaseGridPage>();
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.CollapseExpandSelfServiceBuilderContextPanelElements(false);
+        }
+        
+        [When(@"User clicks on Expand ""(.*)"" page items button in Self Service Page Builder Context Panel")]
+        public void WhenUserClicksOnExpandThePageItemsButtonInSelfServicePageBuilderContextPanel(String contextPanelPageName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
             _driver.WaitForDataLoading();
-            
-            }
+            dashboardPage.CollapseExpandSelfServiceBuilderPageOnContextPanel(contextPanelPageName, true);
+        }
+
+        [When(@"User clicks on Collapse ""(.*)"" page items button in Self Service Page Builder Context Panel")]
+        public void WhenUserClicksOnCollapseThePageItemsButtonInSelfServicePageBuilderContextPanel(String contextPanelPageName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.CollapseExpandSelfServiceBuilderPageOnContextPanel(contextPanelPageName, false);
+        }
+
+        [When(@"User clicks on Add Item button of ""(.*)"" page on Self Service Page Builder Context Panel")]
+        public void WhenUserClicksOnAddItemButtonOfThePageOnSelfServicePageBuilderContextPanel(String contextPanelPageName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.ClickOnContextPanelPageAddItemButton(contextPanelPageName);
+        }
+
+        [When(@"User clicks on Cog Menu button of ""(.*)"" page on Self Service Page Builder Context Panel")]
+        public void WhenUserClicksOnCogMenuButtonOfThePageOnSelfServicePageBuilderContextPanel(String contextPanelPageName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.ClickOnContextPanelPageCogMenuButton(contextPanelPageName);
+        }
+
+        [When(@"User clicks on Cog Menu button of ""(.*)"" page with ""(.*)"" component heading and ""(.*)"" component name on Self Service Page Builder Context Panel")]
+        public void WhenUserClicksOnCogMenuButtonOfThePageWithTheComponentHeadinAndNameOnSelfServicePageBuilderContextPanel(String contextPanelPageName, String componentHeading, String componentName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.ClickOnContextPanelPageSubCogMenuButton(contextPanelPageName, componentHeading, componentName);
+        }
+
+        [When(@"User enters ""(.*)"" text in Self Service Page Builder Context Panel Search Field")]
+        public void WhenUserEntersTextInSelfServicePageBuilderContextPanelSearchField(string searchedText)
+        {
+            var listElement = _driver.NowAt<CustomListElement>();
+            _driver.WaitForElementToBeDisplayed(listElement.ListPanelSearchTextBox);
+            listElement.ListPanelSearchTextBox.Clear();
+            listElement.ListPanelSearchTextBox.SendKeys(searchedText);
+        }
+
+        [When(@"User clicks Close Self Service Page Builder Context Panel button")]
+        public void WhenUserClicksCloseSelfServicePageBuilderContextPanelButton()
+        {
+            var button = _driver.NowAt<BaseDashboardPage>();
+            button.ClosePanelButton.Click();
+        }
+
+        [When(@"User clicks '(.*)' option in opened Cog-menu")]
+        public void WhenUserClicksOptionInOpenedCogMenu(string option)
+        {
+            var cogMenu = _driver.NowAt<CogMenuElements>();
+            _driver.WaitForElementToBeDisplayed(cogMenu.CogMenuList);
+            cogMenu.GetCogMenuOptionByName(option).Click();
+            _driver.WaitForDataLoading();
         }
     }
 }
