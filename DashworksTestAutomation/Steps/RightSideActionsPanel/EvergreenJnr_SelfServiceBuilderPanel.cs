3f1003c92004f5d038677c517e8e285e25ae1586
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-12-02 19:27:28 +0200

    Some steps were renamed in EvergreenJnr_SelfServiceBuilderPanel class

---
+++ DashworksTestAutomation/Steps/RightSideActionsPanel/EvergreenJnr_SelfServiceBuilderPanel.cs
@@ -0,0 +1,55 @@
+﻿using DashworksTestAutomation.Extensions;
+using DashworksTestAutomation.Pages.Evergreen.RightSideActionPanels;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.RightSideActionsPanel
+{
+    [Binding]
+    class EvergreenJnr_SelfServiceBuilderPanel : SpecFlowContext
+    {
+        private readonly RemoteWebDriver _driver;
+
+        public EvergreenJnr_SelfServiceBuilderPanel(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [When(@"User clicks Expand All Sections button on Self Service Builder Panel")]
+        public void WhenUserClicksOnExpandSectionsAllInSelfServiceBuilderContextPanel()
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            _driver.WaitForDataLoading();
+            dashboardPage.CollapseExpandAllElementsOnSelfServiceBuilderContextPanel(true);
+        }
+
+        [When(@"User clicks on Collapse All Sections button on Self Service Builder Panel")]
+        public void WhenUserClicksOnCollapseAllSectionsInSelfServiceBuilderContextPanel()
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.CollapseExpandAllElementsOnSelfServiceBuilderContextPanel(false);
+        }
+
+        [When(@"User clicks on Expand button for item with '(.*)' type and '(.*)' name on Self Service Builder Panel")]
+        public void WhenUserClicksOnExpandButtonForItemWithTypeAndNameOnSelfServiceBuilderPanel(string contextPanelType, string contextPanelName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            _driver.WaitForDataLoading();
+            dashboardPage.CollapseExpandSelfServiceBuilderPageOnContextPanel(true, contextPanelType, contextPanelName);
+        }
+
+        [When(@"User clicks on Collapse button for item with '(.*)' type and '(.*)' name on Self Service Builder Panel")]
+        public void WhenUserClicksOnCollapseButtonForItemWithTypeAndNameOnSelfServiceBuilderPanel(string contextPanelType, string contextPanelName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.CollapseExpandSelfServiceBuilderPageOnContextPanel(false, contextPanelType, contextPanelName);
+        }
+
+        [When(@"User clicks on Add Item button for item with '(.*)' type and '(.*)' name on Self Service Builder Panel")]
+        public void WhenUserClicksOnAddItemButtonForItemWithTypeAndNameOnSelfServiceBuilderPanel(string contextPanelType, string contextPanelName)
+        {
+            var dashboardPage = _driver.NowAt<SelfServiceBuilderContextPanel>();
+            dashboardPage.ContextPanelPageAddItemButton(contextPanelType, contextPanelName).Click();
+        }
+    }
+}
