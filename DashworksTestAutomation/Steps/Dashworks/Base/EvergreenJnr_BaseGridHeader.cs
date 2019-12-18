cb978ffc0423af07a2a2bbdf2d0aa5374efee555
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2019-12-18 13:22:17 +0200

    Added EvergreenJnr_BaseGridHeader class

---
+++ DashworksTestAutomation/Steps/Dashworks/Base/EvergreenJnr_BaseGridHeader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.Dashworks.Base
+{
+    [Binding]
+    public class EvergreenJnr_BaseGridHeader : SpecFlowContext
+    {
+        private readonly RemoteWebDriver _driver;
+
+        public EvergreenJnr_BaseGridHeader(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
