a5c6bf6b99446f05b140aaa9ffc436117ce1479e
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2020-01-10 00:40:11 +0200

    Removed not used namespaces

--- DashworksTestAutomation/Steps/Dashworks/AdminPage/EvergreenJnr_AdminPage_Messages.cs
+++ DashworksTestAutomation/Steps/Dashworks/AdminPage/EvergreenJnr_AdminPage_Messages.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using DashworksTestAutomation.DTO.RuntimeVariables;
 using DashworksTestAutomation.Extensions;
 using DashworksTestAutomation.Pages.Evergreen.AdminDetailsPages;
 using DashworksTestAutomation.Pages.Evergreen.Base;
