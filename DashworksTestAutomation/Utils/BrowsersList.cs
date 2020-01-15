560a1efecf7eeb7d8b56b9a1a5c122a5de61a132
Vitalii Dolotov <vitaliy.dolotov@juribacom>
2020-01-15 06:40:44 +0200

    Fix for  EvergreenJnr_AdminPage_CheckSavingOfChangesOnScopeDetailsPageForDeviceAndMailboxProject test

--- DashworksTestAutomation/Utils/BrowsersList.cs
+++ DashworksTestAutomation/Utils/BrowsersList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DashworksTestAutomation.Extensions;
 using OpenQA.Selenium;
@@ -11,33 +12,66 @@ namespace DashworksTestAutomation.Utils
 {
     public class BrowsersList
     {
-        public List<RemoteWebDriver> Drivers { get; set; }
+        private List<RemoteWebDriver> _drivers = new List<RemoteWebDriver>();
+
+        private Thread _pingDriversThread = null;
 
         private RemoteWebDriver DriverInUse { get; set; }
 
         //id starts from 0 where zero is the main browser
         public RemoteWebDriver GetBrowser(int id)
         {
-            if (id > Drivers.Count - 1)
+            //Drop current driver
+            DriverInUse = null;
+
+            if (id > _drivers.Count - 1)
             {
                 throw new Exception($"Unable to get driver with {id} id");
             }
 
-            DriverInUse = Drivers[id];
+            //Set new current driver
+            DriverInUse = _drivers[id];
+
+            //Start ping thread if not yet started
+            if (_pingDriversThread.Equals(null))
+            {
+                _pingDriversThread = new Thread(PingDrivers);
+            }
 
             return DriverInUse;
+        }
+
+        public void AddDriver(RemoteWebDriver driver)
+        {
+            _drivers.Add(driver);
+        }
+
+        public List<RemoteWebDriver> GetAllBrowsers()
+        {
+            //Stop ping
+            _pingDriversThread?.Abort();
+            return _drivers;
         }
 
         public void PingDrivers()
         {
             try
             {
-                var driversForPing = Drivers.Where(x => !x.Equals(DriverInUse));
+                var driversForPing = _drivers.Where(x => !x.Equals(DriverInUse));
 
                 foreach (RemoteWebDriver driver in driversForPing)
                 {
-                    driver.IsElementExists(By.XPath(".//body"));
+                    try
+                    {
+                        driver.IsElementExists(By.XPath(".//body"));
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Write(e);
+                    }
                 }
+
+                Thread.Sleep(20000);
             }
             catch (Exception e)
             {
