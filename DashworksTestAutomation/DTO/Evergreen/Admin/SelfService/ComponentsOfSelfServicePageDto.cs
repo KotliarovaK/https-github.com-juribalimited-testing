ec8db3ab6f7f440888a0634ba1f7faa2c015032e
andrewd <Andrew.Dzimiskyi@juriba.com>
2020-01-16 16:30:00 +0200

    ComponentsOfSelfServicePageDto was Extended

--- DashworksTestAutomation/DTO/Evergreen/Admin/SelfService/ComponentsOfSelfServicePageDto.cs
+++ DashworksTestAutomation/DTO/Evergreen/Admin/SelfService/ComponentsOfSelfServicePageDto.cs
@@ -36,5 +36,38 @@ namespace DashworksTestAutomation.DTO.Evergreen.Admin.SelfService
 
         [JsonProperty("showInSelfService")]
         public int ShowInSelfService { get; set; }
+
+        [JsonProperty("components")]
+        public List<Component> Components { get; set; }
+
+        public partial class Component
+        {
+            [JsonProperty("componentId")]
+            public int ComponentId { get; set; }
+
+            [JsonProperty("pageId")]
+            public int PageId { get; set; }
+
+            [JsonProperty("componentTypeId")]
+            public int ComponentTypeId { get; set; }
+
+            [JsonProperty("order")]
+            public string Order { get; set; }
+
+            [JsonProperty("componentName")]
+            public string ComponentName { get; set; }
+
+            [JsonProperty("helpText")]
+            public string HelpText { get; set; }
+
+            [JsonProperty("componentType")]
+            public int ComponentType { get; set; }
+
+            [JsonProperty("componentTypeDescription")]
+            public int ComponentTypeDescription { get; set; }
+
+            [JsonProperty("showInSelfService")]
+            public int ShowInSelfService { get; set; }
+        }
     }
 }
