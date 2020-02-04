80c921b13c545eb4e07ca12e7bbdeaa1fc97f733
Anna Ilchenko <anna.ilchenko@juriba.com>
2020-02-04 23:53:02 +0200

    upd

--- DashworksTestAutomation/Pages/Evergreen/PermissionsElement.cs
+++ DashworksTestAutomation/Pages/Evergreen/PermissionsElement.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using DashworksTestAutomation.Base;
 using DashworksTestAutomation.Extensions;
+using DashworksTestAutomation.Pages.Evergreen.Base;
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
 
 namespace DashworksTestAutomation.Pages.Evergreen
 {
-    internal class PermissionsElement : SeleniumBasePage
+    internal class PermissionsElement : BaseRightSideActionsPanel
     {
         [FindsBy(How = How.XPath, Using = ".//div[contains(@class, 'context-container')]//div[contains(@class, 'header')]/span[contains(text(), 'Permissions')]")]
         public IWebElement PermissionsPanel { get; set; }
