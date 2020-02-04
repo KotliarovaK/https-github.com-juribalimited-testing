bbd6b5de12b637721bf6e8a40cd7ff83a247c705
szinkovskiy <sergey.zinkovskiy@doublegood.com>
2020-02-04 22:38:15 +0200

    sz: added expand first section for dashboard page

--- DashworksTestAutomation/Steps/Dashworks/Dashboards/EvergreenJnr_DashboardsPage.cs
+++ DashworksTestAutomation/Steps/Dashworks/Dashboards/EvergreenJnr_DashboardsPage.cs
@@ -44,6 +44,9 @@ namespace DashworksTestAutomation.Steps.Dashworks
             var page = _driver.NowAt<EvergreenDashboardsPage>();
             _driver.WaitForDataLoading();
             page.EditModeOnOffTrigger.Click();
+            //TODO: expand section by index
+            page.SectionsBlocks.First().Click();
+            _driver.WaitForElementToContainsTextInAttribute(page.SectionsBlocks.First(), "true", "aria-expanded");
         }
 
         [When(@"User clicks Dashboards Details icon on Dashboards page")]
