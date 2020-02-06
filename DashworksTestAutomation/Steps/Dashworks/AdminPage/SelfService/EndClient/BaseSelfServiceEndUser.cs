753d0eb77001cd313576f980041d52ed6e535f36
andrewd <Andrew.Dzimiskyi@juriba.com>
2020-02-06 10:28:16 +0200

    middle work

---
+++ DashworksTestAutomation/Steps/Dashworks/AdminPage/SelfService/EndClient/BaseSelfServiceEndUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DashworksTestAutomation.Extensions;
+using DashworksTestAutomation.Pages.Evergreen.AdminDetailsPages.SelfService.EndClient;
+using DashworksTestAutomation.Utils;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DashworksTestAutomation.Steps.Dashworks.AdminPage.SelfService.EndClient
+{
+    [Binding]
+    class BaseSelfServiceEndUser : SpecFlowContext
+    {
+        private readonly RemoteWebDriver _driver;
+        public BaseSelfServiceEndUser(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [Then(@"Self Service Tools Panel is displayed for end client")]
+        public void ThenSelfServiceToolsPanelIsDisplayedForEndClient()
+        {
+            var page = _driver.NowAt<SelfServiceEndClientBasePage>();
+            Verify.IsTrue(page.SelfServiceToolsPanel.IsElementDisplayed())
+        }
+    }
+}
