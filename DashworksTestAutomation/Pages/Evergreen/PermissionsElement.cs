34877ed85fd1cb6853878ea36d678ac0c67f5fa7
szinkovskiy <sergey.zinkovskiy@doublegood.com>
2020-02-13 12:36:41 +0200

    build fix

--- DashworksTestAutomation/Pages/Evergreen/PermissionsElement.cs
+++ DashworksTestAutomation/Pages/Evergreen/PermissionsElement.cs
@@ -19,6 +19,25 @@ namespace DashworksTestAutomation.Pages.Evergreen
         [FindsBy(How = How.XPath, Using = "//div[@role='listbox']")]
         public IWebElement SharingUserList { get; set; }
 
+        [FindsBy(How = How.XPath, Using = ".//input[@aria-label='User']")]
+        public IWebElement SelectUserDropdown { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//td[@class='userName']")]
+        public IList<IWebElement> PermissionAddedUser { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//td[@class='permission']")]
+        public IList<IWebElement> PermissionTypeOfAccess { get; set; }
+
+        //TODO will be replaced COG MENU element
+        public IWebElement GetMenuOfSharedUser(string username)
+        {
+            var sharedUserCogMenu =
+                By.XPath($".//td[contains(text(),'{username}')]/following-sibling::td/div[starts-with(@class, 'cog-menu')]//i");
+            Driver.MouseHover(sharedUserCogMenu);
+            Driver.WaitForElementToBeDisplayed(sharedUserCogMenu);
+            return Driver.FindElement(sharedUserCogMenu);
+        }
+
         public IWebElement GetSharingUserOnDetailsPanelByName(string userName)
         {
             var selector = By.XPath($".//tr[contains(@class, 'menu-show-on-hover')]//td[text()='{userName}']");
