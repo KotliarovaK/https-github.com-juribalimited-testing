1105307c8ba5ce84c74585a62a2a4580a54f8b3d
szinkovskiy <sergey.zinkovskiy@doublegood.com>
2020-02-24 10:06:01 +0200

    update verify

--- DashworksTestAutomation/Steps/Dashworks/Base/EvergreenJnr_BaseGrid.cs
+++ DashworksTestAutomation/Steps/Dashworks/Base/EvergreenJnr_BaseGrid.cs
@@ -52,7 +52,7 @@ namespace DashworksTestAutomation.Steps.Dashworks.Base
             var page = _driver.NowAt<BaseGridPage>();
             var allHeaders = page.GetAllHeadersText();
             var expectedList = table.Rows.SelectMany(row => row.Values).ToList();
-            Verify.AreEqual(allHeaders, expectedList, "Columns order is incorrect");
+            Verify.AreEqual(expectedList, allHeaders, "Columns order is incorrect");
         }
 
         [Then(@"All column headers are unique")]
