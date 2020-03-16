a84473d1f73e1121438e1802e56ac9148e6ef575
Vitalii Dolotov <vitalii.dolotov@juriba.com>
2020-03-16 12:59:45 +0200

    Renamed password configuration setting

--- DashworksTestAutomationCore/Providers/UserProvider.cs
+++ DashworksTestAutomationCore/Providers/UserProvider.cs
@@ -20,7 +20,7 @@ namespace DashworksTestAutomation.Providers
 
         //private static int _iter;
 
-        public static string Password = ConfigReader.ByKey("user.password");
+        public static string Password = ConfigReader.ByKey("userPassword");
 
         private static readonly List<UserDto> _accounts = new List<UserDto> { };
 
@@ -39,7 +39,7 @@ namespace DashworksTestAutomation.Providers
         {
             return new UserDto()
             {
-                Username = $"admin",
+                Username = ConfigReader.ByKey("userLogin"),
                 Password = Password,
                 Language = DefaultUserLanguage
             };
