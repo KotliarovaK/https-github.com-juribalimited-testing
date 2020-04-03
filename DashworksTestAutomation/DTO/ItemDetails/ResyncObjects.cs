969564ea582f85801b969fb1e83093acf9697b68
Vitalii Dolotov <vitalii.dolotov@juriba.com>
2020-04-03 16:32:08 +0300

    Added ResyncItems afterscenario

---
+++ DashworksTestAutomation/DTO/ItemDetails/ResyncObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashworksTestAutomation.DTO.ItemDetails
+{
+    public class ResyncObjects
+    {
+        public List<ResyncItemst> Value { get; set; }
+
+        public ResyncObjects()
+        {
+            Value = new List<ResyncItemst>();
+        }
+    }
+
+    public class ResyncItemst
+    {
+        public string List { get; set; }
+        public string ProjectName { get; set; }
+        public List<string> Objects { get; set; }
+    }
+}
