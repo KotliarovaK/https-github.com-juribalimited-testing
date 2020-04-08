63b7935540b1c5a0a00939cafcdf114618b97fe6
Anna Ilchenko <anna.ilchenko@juriba.com>
2020-04-08 15:34:30 +0300

    upd

--- DashworksTestAutomation/Steps/Dashworks/Base/EvergreenJnr_BasePage.cs
+++ DashworksTestAutomation/Steps/Dashworks/Base/EvergreenJnr_BasePage.cs
@@ -269,7 +269,7 @@ namespace DashworksTestAutomation.Steps.Dashworks.Base
                 var textbox = page.GetTextbox(placeholder);
                 textbox.Clear();
                 textbox.SendKeys(value);
-                //Verify.IsTrue(page.IsAutocompleteResultsCountMessageDisplayed(), $"Some checkboxes found for '{placeholder}' autocomplete");
+                Verify.IsTrue(page.IsAutocompleteResultsCountMessageDisplayed(), $"Some checkboxes found for '{placeholder}' autocomplete");
                 Verify.IsFalse(page.IsAutocompleteCheckboxDisplayed(value), $"'{value}' checkbox is displayed, but should not");
                 page.BodyContainer.Click();
             }
