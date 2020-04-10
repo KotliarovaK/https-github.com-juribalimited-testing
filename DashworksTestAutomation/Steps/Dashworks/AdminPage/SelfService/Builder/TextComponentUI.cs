c3f5a445fcf4cad1779f23bed2daf7ebcaa637df
OleksandrMishchenkoGit <60176309+OleksandrMishchenkoGit@users.noreply.github.com>
2020-04-10 18:37:33 +0300

    update method name

--- DashworksTestAutomation/Steps/Dashworks/AdminPage/SelfService/Builder/TextComponentUI.cs
+++ DashworksTestAutomation/Steps/Dashworks/AdminPage/SelfService/Builder/TextComponentUI.cs
@@ -34,7 +34,7 @@ namespace DashworksTestAutomation.Steps.Dashworks.AdminPage.SelfService.Builder
         }
 
         [Then(@"header format options are displayed on the text component page")]
-        public void ThenFormattingOpe(Table table)
+        public void ThenHeaderFormatOPtionsAreDisplayedOnTheTextComponentPage(Table table)
         {
             var page = _driver.NowAt<TextComponentPage>();
             _driver.WaitForElementToBeDisplayed(page.ToolbarWithFormattingOptions);
