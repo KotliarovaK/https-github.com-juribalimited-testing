913f9eaac52985df40e4155a6475f7f70fe1ae1a
Anna Ilchenko <anna.ilchenko@juriba.com>
2020-04-21 10:59:30 +0300

    upd

--- DashworksTestAutomationCore/Steps/Dashworks/Base/EvergreenJnr_BasePage.cs
+++ DashworksTestAutomationCore/Steps/Dashworks/Base/EvergreenJnr_BasePage.cs
@@ -299,8 +299,6 @@ namespace DashworksTestAutomation.Steps.Dashworks.Base
                 var textbox = page.GetTextbox(placeholder);
                 textbox.Clear();
                 textbox.SendKeys(value);
-                //AnnI: the count of items is less than 50, no labels are shown (DAS-18781)
-                //Verify.IsTrue(page.IsAutocompleteResultsCountMessageDisplayed(), $"Some checkboxes found for '{placeholder}' autocomplete");
                 Verify.IsFalse(page.IsAutocompleteCheckboxDisplayed(value), $"'{value}' checkbox is displayed, but should not");
                 page.BodyContainer.Click();
             }
@@ -316,8 +314,6 @@ namespace DashworksTestAutomation.Steps.Dashworks.Base
                 var textbox = page.GetTextbox(placeholder);
                 textbox.Clear();
                 textbox.SendKeys(value);
-                //AnnI: the count of items is less than 50, no labels are shown (DAS-18781)
-                //Verify.IsFalse(page.IsAutocompleteResultsCountMessageDisplayed(), $"Some checkboxes not found for '{placeholder}' autocomplete");
                 Verify.IsTrue(page.IsAutocompleteCheckboxDisplayed(value), $"'{value}' checkbox is missed");
                 page.BodyContainer.Click();
             }
