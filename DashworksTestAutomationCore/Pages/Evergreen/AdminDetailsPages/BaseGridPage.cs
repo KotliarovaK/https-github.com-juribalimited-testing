5e11511d7f3b4b761d485115367d2981f0d5a792
Anna Ilchenko <anna.ilchenko@juriba.com>
2020-05-14 10:11:56 +0300

    upd selector

--- DashworksTestAutomationCore/Pages/Evergreen/AdminDetailsPages/BaseGridPage.cs
+++ DashworksTestAutomationCore/Pages/Evergreen/AdminDetailsPages/BaseGridPage.cs
@@ -212,7 +212,7 @@ namespace DashworksTestAutomation.Pages.Evergreen.AdminDetailsPages
         private string ActionElementSelector(string columnName)
         {
             var results =
-                $".//div[@role='presentation']//div[contains(@class,'filter')]/div[{GetColumnNumberByName(columnName)}]//div[contains(@ref,'eFloatingFilterBody')]";
+                $".//div[@role='presentation']//div[contains(@class,'filter')]/div[{GetColumnNumberByName(columnName)}]//div[contains(@ref,'eFloatingFilterBody')] | .//div[@role='presentation']//div[@class='ag-header-row'][2]/div[{GetColumnNumberByName(columnName)}]";
             return results;
         }
 
